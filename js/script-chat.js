b1dc2ec615a6b8b892344e39f1bc3c8786fe12cb
Piotr Juskowiak <piotrjuskowiak@macbook.local>
2021-02-01 00:54:15 +0100

    asd

---
+++ js/script-chat.js
@@ -0,0 +1,12 @@
+! function(g, s, q, r, d) {
+    r = g[r] = g[r] || function() {
+        (r.q = r.q || []).push(
+            arguments)
+    };
+    d = s.createElement(q);
+    q = s.getElementsByTagName(q)[0];
+    q.parentNode.
+    insertBefore(d, q)
+}(window, document, 'script', '_gs');
+
+_gs('GSN-428181-C');
