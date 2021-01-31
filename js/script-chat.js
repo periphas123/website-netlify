cdb547a0b40e4590b00fdb42c41c001880f443a2
Piotr Juskowiak <piotrjuskowiak@macbook.local>
2021-02-01 00:54:49 +0100

    asd

--- js/script-chat.js
+++ js/script-chat.js
@@ -5,6 +5,7 @@
     };
     d = s.createElement(q);
     q = s.getElementsByTagName(q)[0];
+    d.src = '//d1l6p2sc9645hc.cloudfront.net/tracker.js';
     q.parentNode.
     insertBefore(d, q)
 }(window, document, 'script', '_gs');
